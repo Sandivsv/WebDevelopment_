783afab210c135d4a7a0d192150b6814feba2804
Sandivsv <sandeepkmr7379@gmail.com>
2022-03-13 02:01:36 +0530

    Open File with the help of execSync

--- module2/node/childProcess.js
+++ module2/node/childProcess.js
@@ -1,15 +1,17 @@
 let cp = require('child_process');
-const { fileURLToPath } = require('url');
-console.log(cp); 
+// console.log(cp); 
+
 
 // open calculator with help child process
-
 //  cp.execFileSync("calc");
 
 
-//  print output of any fileURLToPath
+// open vscode with ChildProcess
+// cp.execSync("code");
 
-let output=cp.execFileSync("node abc.js");
-console.log("Let's run abc file to test: ",output);
 
-// last two line show console.error
+//  print output of any file 
+let output=cp.execSync("node abc.js");
+console.log("Let's run abc file to test: " + output);
+
+ 
