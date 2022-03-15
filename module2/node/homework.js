3d71c09c00721fbb010a8536f3042d6bd5b1ae83
Sandivsv <sandeepkmr7379@gmail.com>
2022-03-15 17:12:13 +0530

    HW to create FIle and Folder

---
+++ module2/node/homework.js
@@ -0,0 +1,38 @@
+// HOMEWORK 
+
+let fs = require('fs');
+let path = require('path');
+
+let filepath=__dirname    //find path
+console.log(filepath);
+
+
+// create a directory or folder 
+    //  if file does not exist then through error we handle by checking existing file 
+if(!fs.existsSync("New Folderdir")){
+    fs.mkdirSync("New Folderdir");
+}
+
+// if we want to give path for creating folder 
+// fs.mkdirSync(filepath,"myFile");      // Error to give path.
+
+
+// read that directory (.readdirSync)
+let contentOfFolder=fs.readdirSync(filepath);
+console.log(contentOfFolder);
+
+
+// delete that directory 
+// fs.rmdirSync("New Folderdir");
+
+
+
+// copy a file 
+
+let sourcePath = path.join(__dirname,"copy.txt");
+let destinationPath = path.join(__dirname,"ghostFolder", "copy.txt");
+
+console.log(sourcePath);
+console.log(destinationPath);
+
+fs.copyFileSync(sourcePath, destinationPath);
---
+++ module2/node/testModule.js
@@ -0,0 +1,7 @@
+
+// give path in require 
+let calc = require('./calculator.js');
+// console.log(calc);
+
+console.log(calc.addition(3,4));
+console.log(calc.multiply(5,5));
