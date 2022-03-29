99cbf461608510bdeb4ed6629bb542f3f79afbfc
Sandivsv <sandeepkmr7379@gmail.com>
2022-03-29 22:50:11 +0530

    Highest Wicket taker Scrapping done

--- module2/WebScrapping/highestWicketTaker.js
+++ module2/WebScrapping/highestWicketTaker.js
@@ -4,7 +4,7 @@ const request = require('request');
 const jsdom = require('jsdom');
 const {JSDOM} = jsdom;
 
-const link="https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/chennai-super-kings-vs-kolkata-knight-riders-1st-match-1304047/live-cricket-score"
+const link="https://www.espncricinfo.com/series/ipl-2021-1249214/royal-challengers-bangalore-vs-kolkata-knight-riders-eliminator-1254115/full-scorecard"
 request(link, cb);
 
 function cb(error, response, html){
@@ -17,12 +17,36 @@ function cb(error, response, html){
         // let name = documents.querySelector(".team.team-gray .name-link .name");
         // console.log(name.textContent);
 
-        let name = documents.querySelectorAll(".team .name-link .name");
-        console.log(name[0].textContent);
-        console.log(name[1].textContent);
+        let bowlerTables = documents.querySelectorAll(".table.bowler");
+        // console.log(bowlerTables[0].textContent);
+        // console.log(bowlerTables.length);
 
-        let res = documents.querySelector(".event .status-text span");
-        console.log(res.textContent);
+        let highWikt=0;   //for count highest Wicket
+        let name ="";
+
+        for(let i=0; i<bowlerTables.length; i++){
+            let rows = bowlerTables[i].querySelectorAll("tbody tr");
+            // console.log(rows.length);
+
+            for(let j=0; j<rows.length; j++){
+                let tds = rows[j].querySelectorAll("td");
+
+                // in table data some empty data rows so...
+                if(tds.length > 1){        
+                    // we only take name and wicket which is on 0 and 4th index 
+                    console.log("Name-: ", tds[0].textContent, ", Number of Wicket -: ", tds[4].textContent);
+                    if(highWikt < tds[4].textContent){
+                        highWikt = tds[4].textContent;
+                        name = tds[0].textContent;
+                    }
+                }
+            }
+        }
+
+        console.log("*******************************************************");
+        console.log("Maximum Wicket-:", highWikt, "taken By '", name, "'\n");   // Highest Wicket taken and his Name
+
+
         
     }
 }
