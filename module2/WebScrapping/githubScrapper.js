00200847fa0121c49f962d104d36969295a177fa
Sandivsv <sandeepkmr7379@gmail.com>
2022-04-10 16:33:11 +0530

    Homework of Scrapping Github Repo Issues

---
+++ module2/WebScrapping/githubScrapper.js
@@ -0,0 +1,4 @@
+                                                        // Homework
+
+// go on this link ("https://github.com/topics") and choose first 3 topics and go on all their topic site. 
+// And after it find min 8 repo related to to that topic and go on Issues and make a json file of taking 5 issues with issues name and link  
