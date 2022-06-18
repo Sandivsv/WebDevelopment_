44381872158351bab451af55e93e55e49a14c76d
Sandivsv <sandeepkmr7379@gmail.com>
2022-06-18 22:54:36 +0530

    Todo app with React

---
+++ module4/react/todo-app/src/components/Todo.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import "./Todo.css"
+
+class Todo extends Component{
+    constructor(){
+        super();
+        this.state = {
+            tasks:["Learn JS","Learn DOM","Learn React","Learn Node"],
+            currTask:""
+        }
+    }
+    
+    handleChange = (e)=>{
+        // console.log(e.target.value);
+        this.setState({
+            currTask:e.target.value
+        })
+    }
+
+    handleAddTask = ()=>{
+        this.setState({
+            tasks:[...this.state.tasks,this.state.currTask],
+            currTask:""
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <input type="text" value={this.state.currTask} onChange={this.handleChange}/>
+                <button onClick={this.handleAddTask}>Add Task</button>
+                <ul>
+                    {this.state.tasks.map((ele)=>(
+                        <li>
+                            <p>{ele}</p>
+                            <button>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Todo;
+
+npx create-react-app movies-app
