81da9b63f57e158d841293ef9081d450f9b29e6b
Sandivsv <sandeepkmr7379@gmail.com>
2022-06-21 23:59:58 +0530

    Pagenation and hover effect of card

--- module4/react/movies-app/src/components/movieList.js
+++ module4/react/movies-app/src/components/movieList.js
@@ -1,28 +1,49 @@
 import { Component } from "react";
-import {movies} from "../movieData"
+import { movies } from "../movieData"
 
-class MovieList extends Component{
-    render(){
+class MovieList extends Component {
+    constructor(){
+        super();
+        this.state={
+            hover:"",
+            pArr :[1]
+        };
+    }
+
+    render() {
         let moviesArr = movies.results
-        return(
+        return (
             <>
-            <div>
-                <h3 className="text-center"><strong>Trending</strong></h3>
-            </div>
-            <div className="movies-list">
-                {moviesArr.map((movieEle)=>(
-                    <div className="card movie-card">
-                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" style={{height:'40vh',width:'20vw'}} className="card-img-top movie-img" alt="..."/>
-                    <div className="card-body">
-                      <h5 className="card-title">{movieEle.original_title}</h5>
-                    </div>
-                  </div>
-
-                ))}
-            </div>
+                <div>
+                    <h3 className="text-center"><strong>Trending</strong></h3>
+                </div>
+                <div className="movies-list">
+                    {moviesArr.map((movieEle) => (
+                        <div className="card movie-card" onMouseEnter={()=>this.setState({hover:movieEle.id})} onMouseLeave={()=>this.setState({hover:""})} >
+                            <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{ height: '45vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
+                            <h5 className="card-title movie-title">{movieEle.title}</h5>
+                            <div style={{ display: 'flex', justifyContent: "center" }}>
+                                {this.state.hover === movieEle.id && (
+                                <a href=" " type="button" className="btn btn-primary movies-button">Add to Favourites</a>)}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div style={{display:"flex",justifyContent:"center"}}>
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item"><a className="page-link" href=" ">Previous</a></li>
+                        {this.state.pArr.map((ele)=>(
+                            <li className="page-item"><a className="page-link" href=" ">{ele}</a></li>
+                        ))}
+                        
+                        <li className="page-item"><a className="page-link" href=" ">Next</a></li>
+                    </ul>
+                </nav>
+                </div>
             </>
         )
     }
 }
 
-export default MovieList 
+export default MovieList
